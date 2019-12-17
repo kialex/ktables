bf82af2449d5f808285c57fec5358731774fa5c4
Vladislav Dneprov <vladislav.dneprov@splynx.com>
2019-12-18 00:55:20 +0200

    init skeleton

--- public/index.php
+++ public/index.php
@@ -1,9 +1,7 @@
 <?php
-
 use App\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
-
 require dirname(__DIR__).'/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
