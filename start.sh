0bc71c068a370433dbe39d687ad757f2cb9c7f5a
Vladislav Dneprov <vladislav.dneprov@splynx.com>
2019-12-20 00:42:50 +0200

    start to add user component

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-export CURRENT_UID=$(id -u):$(id -g)
+if !(grep -q CURRENT_UI ".env"); then
+    echo CURRENT_UID=$(id -u):$(id -g)>>.env
+fi
 
 docker-compose build --no-cache
 docker-compose up
